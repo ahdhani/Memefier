f616b34da67a97632b8f5d7495059e2e18af29f5
Sarath C Ani <mailsforsarath98@gmail.com>
2020-05-16 13:42:44 +0530

    Auth redux added

--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
 import testReducer from './test/testReducer'
+import authReducer from './auth/authReducer'
 
 const rootReducer = combineReducers({
-    test : testReducer
+    test : testReducer,
+    auth : authReducer
 })
 
 export default rootReducer
