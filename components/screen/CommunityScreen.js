fab0398336e82f489f7ab6a10f19407de4c0ebd1
Sarath C Ani <mailsforsarath98@gmail.com>
2020-05-27 23:55:41 +0530

    fetchUsers completed

--- components/screen/CommunityScreen.js
+++ components/screen/CommunityScreen.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Image, FlatList } from 'react-native'
 import { Container, Button, Item, Text, ListItem, Input, Header, Content, Left, Picker, Icon, Body, Right, H3, H2, DatePicker, Title, Thumbnail } from 'native-base'
+import { connect } from 'react-redux'
+import { db } from '../../config'
 
-
-export default class CommunityScreen extends Component {
+class CommunityScreen extends Component {
 
     state = {
-        users: [
-            {
-                username: 'hani',
-                avatar: null,
-                isFollow: false,
-            },
-            {
-                username: 'kudu',
-                avatar: null,
-                isFollow: false,
-            },
-        ]
+        users: []
+    }
+
+    componentDidMount = () => {
+        console.log("COMPONENT DID MOUNTED (FEED SCREEN)");
+        // this.props.fetchPosts();
+        this.fetchUsers()
+    }
+
+    fetchUsers = () => {
+        var user_list = []
+        db.collection('userDetails')
+            .get()
+            .then(snapshot => {
+                snapshot.docs.forEach(user => {
+                
+                    user_list = [{...user.data() , uid : user.id} , ...user_list]
+                })
+
+                this.setState({
+                    users : user_list
+                })
+
+            })
+            .catch(error => console.log("ERR :" , error.message))
     }
 
     toggleFollow = (index) => {
@@ -26,6 +40,16 @@ export default class CommunityScreen extends Component {
         this.setState({
             users: users,
         })
+    }
+
+    followUser = (uid) => {
+        console.log("Follow user clicked");
+        this.props.follow(uid);
+    }
+
+    unfollowUser = (uid) => {
+        console.log("Unfollow clicked");
+        this.props.unfollow(uid);
     }
 
     render() {
@@ -44,10 +68,10 @@ export default class CommunityScreen extends Component {
                         renderItem={({ item , index}) => (
                             <Item style={{ flexDirection: 'row', padding: 4 }}>
                                 <Thumbnail source={{ uri: item.avatar }} />
-                                <Text style={{ marginLeft: 8 }}>{item.username}</Text>
+                                <Text style={{ marginLeft: 8 }}>{item.firstname} {item.lastname}</Text>
                                 <Text style={{ marginLeft: 200 }}
                                         onPress = {()=>this.toggleFollow(index)}
-                                >{item.isFollow ? 'follow ': 'unfollow'}</Text>
+                                >{!(item.uid in this.props.following) ? 'follow ': 'unfollow'}</Text>
                             </Item>
                         )}
                         enableEmptySections={true}
@@ -69,3 +93,19 @@ const styles = StyleSheet.create({
         width: '100%',
     },
 });
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    userDetails: state.auth.userDetails , 
+    following : state.auth.following
+})
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        logoutUser: () => dispatch(logoutUser()) ,
+        unfollow : (user_id) => dispatch(unfollow_user(user_id)) ,
+        follow : (user_id) => dispatch(follow_user(user_id))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommunityScreen)
