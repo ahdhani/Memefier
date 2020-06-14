19d7eb0cd3724981eaae121cc976990b459a465d
ahdhani <ahmedhanitk@gmail.com>
2020-06-14 23:25:41 +0530

    Community tabs enhanced

--- components/screen/CommunityStack.js
+++ components/screen/CommunityStack.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import { Container, Header, Content, Tab, Tabs } from 'native-base';
 import CommunityScreen from './CommunityScreen';
 import ChallengeScreen from './ChallengeScreen';
+import colors from '../../constants/colors'
 // import Tab3 from './tabThree';
 export default class CommunityStack extends Component {
-  render() {
-    return (
-      <Container>
-        {/* <Header hasTabs /> */}
-        <Tabs >
-          <Tab heading="Community">
-            <CommunityScreen />
-          </Tab>
-          <Tab heading="Challenges">
-            <ChallengeScreen />
-          </Tab>
-        </Tabs>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Tabs tabBarPosition='bottom' locked
+                    tabBarUnderlineStyle={{ backgroundColor: 'black', height: 2 }}>
+                    <Tab heading="Community" tabStyle={{ backgroundColor: colors.color5 }}
+                        activeTabStyle={{ backgroundColor: '#142116' }}
+                        activeTextStyle={{ color: colors.color1,fontSize: 20 }}
+                        textStyle={{ color: colors.color3,fontSize: 18 }}>
+                        <CommunityScreen />
+                    </Tab>
+                    <Tab heading="Challenges" tabStyle={{ backgroundColor: colors.color5 }}
+                        activeTabStyle={{ backgroundColor: '#142116' }}
+                        activeTextStyle={{ color: colors.color1,fontSize: 20 }}
+                        textStyle={{ color: colors.color3,fontSize: 18 }}>
+                        <ChallengeScreen />
+                    </Tab>
+                </Tabs>
+            </Container>
+        );
+    }
 }
