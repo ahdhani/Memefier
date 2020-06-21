1976952a17fe41da99be72d209dcc77b95e9a412
shyamjithMC <shyamjithMC@cet.ac.in>
2020-06-21 12:01:52 +0530

    dddd

--- components/screen/ChallengeScreen.js
+++ components/screen/ChallengeScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { View, Image, FlatList, Text } from 'react-native'
 import { Container, Header, Content, Card, CardItem, Left, Right, Body, Title, Button, Tab, Item, Tabs } from 'native-base';
+import colors from '../../constants/colors'
 
 export default class ChallengeScreen extends Component {
   render() {
@@ -17,8 +18,8 @@ export default class ChallengeScreen extends Component {
             </View>
             <Right>
               <Text >Ends in 5hrs</Text>
-              <Button height={30} width={75} onPress={this.onJoin}>    
-              {/*onJoin not defined yet*/}
+              <Button height={30} width={75} onPress={this.onJoin}>
+                {/*onJoin not defined yet*/}
                 <Text style={{ color: "white", fontSize: 17 }}>     Join</Text>
               </Button>
             </Right>
@@ -38,13 +39,28 @@ export default class ChallengeScreen extends Component {
           </Body>
         </Header>
         <Content>
-
           <CardItem style={{ flexDirection: 'column' }}>
             <Button block onPress={this.makeChallenge}>
               {/* makeChallenge not defined yet */}
               <Text style={{ color: "white", fontSize: 18 }}>Make a Challenge</Text>
             </Button>
           </CardItem>
+
+          <Tabs tabBarPosition='bottom' locked
+            tabBarUnderlineStyle={{ backgroundColor: 'white', height: 2 }}>
+            <Tab heading="Community" tabStyle={{ backgroundColor: colors.color5 }}
+              activeTabStyle={{ backgroundColor: '#142116' }}
+              activeTextStyle={{ color: colors.color1, fontSize: 20 }}
+              textStyle={{ color: colors.color3, fontSize: 18 }}>
+              
+            </Tab>
+            <Tab heading="Challenges" tabStyle={{ backgroundColor: colors.color5 }}
+              activeTabStyle={{ backgroundColor: '#142116' }}
+              activeTextStyle={{ color: colors.color1, fontSize: 20 }}
+              textStyle={{ color: colors.color3, fontSize: 18 }}>
+              
+            </Tab>
+          </Tabs>
 
           {challenges}
 
