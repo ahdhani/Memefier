f00417bcb3385157b26863d87d71786fc4a69b02
Sarath C Ani <mailsforsarath98@gmail.com>
2020-07-07 17:22:45 +0530

    algolia frontend finished

--- components/functions/algolia.js
+++ components/functions/algolia.js
@@ -1,18 +1,19 @@
 import { ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY } from 'react-native-dotenv'
 const algoliasearch = require("algoliasearch");
- 
+
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY);
+
 const user = client.initIndex("users");
- 
-export const algoliaPopulateUser = (user_id , user_details) => {
+
+export const algoliaPopulateUser = (user_id, user_details) => {
   const objects = [
     {
       objectID: user_id,
       ...user_details
     }
   ];
-   
-return user
+
+  return user
     .saveObjects(objects)
     .then(({ objectIDs }) => {
       console.log(objectIDs);
@@ -21,6 +22,11 @@ return user
     .catch(err => {
       console.log(err);
     });
+}
+
+export const algoliaSearch = (query) => {
+  return user
+    .search(query)
 }
 /*
 WORKING function algoliaTest
@@ -31,7 +37,7 @@ export const algoliaTest = () => {
           name: "Foo"
         }
       ];
-       
+
     return user
         .saveObjects(objects)
         .then(({ objectIDs }) => {
