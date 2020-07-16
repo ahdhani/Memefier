b06640ffea1ae1934ad09d8f529f8365ed53282a
ahdhani <ahmedhanitk@gmail.com>
2020-07-16 22:35:39 +0530

    Community screen completed

--- components/screen/CommunityScreen/CommunityCard.js
+++ components/screen/CommunityScreen/CommunityCard.js
@@ -1,18 +1,18 @@
 import React, { Component, useEffect, useState } from 'react';
-import { Card, CardItem, Button,  } from 'native-base'
-import { View, Text,} from 'react-native';
+import { Card, CardItem, Button,Thumbnail } from 'native-base'
+import { View, Text, } from 'react-native';
 // import { useNavigation } from '@react-navigation/native';
 import { fetchGroupDetails } from '../../functions/community'
 
 const CommunityCard = props => {
 
-    const [group, setGroup] = useState()
+    const [group, setGroup] = useState({})
 
     useEffect(() => {
-        console.log('grp id',props.group_id)
+        // console.log('grp id',props.group_id)
 
         fetchGroupDetails(props.group_id)
-            .then(res => { 
+            .then(res => {
                 setGroup(res)
                 console.log(res)
             });
@@ -21,21 +21,17 @@ const CommunityCard = props => {
 
     return (
 
-        <CardItem style={{ marginVertical: -5 }}>
-            <Card style={{ padding: 0, borderRadius: 8, flexDirection: 'row', alignItems: 'center' }}
-            >
-                <CardItem style={{ marginLeft: 15 }}>
-                    <Button style={{ borderRadius: 100, alignItems: 'center', justifyContent: 'center' }} height={65} width={65}
-                    // onPress={() => this.props.navigation.navigate('CommunityFeed')} 
-                    >
-                    </Button>
-                    <View style={{ marginLeft: 15 }}>
-                        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>zfzsf</Text>
-                        <Text style={{ color: 'rgba(0,0,0,0.35)' }}>dzxfg members</Text>
-                    </View>
-                </CardItem>
-            </Card>
-        </CardItem>
+        // <CardItem style={{ marginVertical: -5}}>
+        <Card style={{ borderRadius: 8, flexDirection: 'row', alignItems: 'center' }}>
+            <CardItem style={{ marginLeft: 10, width: '100%' }}>
+                <Thumbnail source={{ uri: group.dp }} style={{ zIndex: 2 }} />
+                <View style={{ marginLeft: 15 }}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{group.name}</Text>
+                    <Text style={{ color: 'rgba(0,0,0,0.35)' }}>{group.members} members</Text>
+                </View>
+            </CardItem>
+        </Card>
+        // </CardItem>
     )
 }
 
