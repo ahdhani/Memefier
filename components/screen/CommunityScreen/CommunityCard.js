85f684139c7d9df16a5eee50b5412ff03141c957
ahdhani <ahmedhanitk@gmail.com>
2020-07-16 22:08:28 +0530

    community card

---
+++ components/screen/CommunityScreen/CommunityCard.js
@@ -0,0 +1,41 @@
+import React, { Component, useEffect, useState } from 'react';
+import { Card, CardItem, Button,  } from 'native-base'
+import { View, Text,} from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+import { fetchGroupDetails } from '../../functions/community'
+
+const CommunityCard = props => {
+
+    const [group, setGroup] = useState()
+
+    useEffect(() => {
+        console.log('grp id',props.group_id)
+        fetchGroupDetails(props.group_id)
+            .then(res => { 
+                setGroup(res)
+                console.log(res)
+            });
+
+    }, []);
+
+    return (
+
+        <CardItem style={{ marginVertical: -5 }}>
+            <Card style={{ padding: 0, borderRadius: 8, flexDirection: 'row', alignItems: 'center' }}
+            >
+                <CardItem style={{ marginLeft: 15 }}>
+                    <Button style={{ borderRadius: 100, alignItems: 'center', justifyContent: 'center' }} height={65} width={65}
+                    // onPress={() => this.props.navigation.navigate('CommunityFeed')} 
+                    >
+                    </Button>
+                    <View style={{ marginLeft: 15 }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>zfzsf</Text>
+                        <Text style={{ color: 'rgba(0,0,0,0.35)' }}>dzxfg members</Text>
+                    </View>
+                </CardItem>
+            </Card>
+        </CardItem>
+    )
+}
+
+export default CommunityCard;
--- components/screen/CommunityScreen/CommunityScreen.js
+++ components/screen/CommunityScreen/CommunityScreen.js
@@ -4,6 +4,7 @@ import { Container, Button, Text, Card, CardItem, Content, Right } from 'native-
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { connect } from 'react-redux';
 import { fetchGroups, joinGroup, fetchGroupDetails } from '../../functions/community'
+import CommunityCard from './CommunityCard';
 
 
 class CommunityScreen extends Component {
@@ -15,77 +16,58 @@ class CommunityScreen extends Component {
     }
 
     componentDidMount = async () => {
-        fetchGroups(this.props.user.uid).then(
-            groups => {
+        fetchGroups(this.props.user.uid)
+            .then(groups => {
+                groups.forEach(group => {
+                    // var data = group.data()
+                    // console.log(data.group_id)
+                    this.setState({ groups: [...this.state.groups, group.data().group_id] })
+                })
+            });
+    }
+    render() {
+        return (
+            <Container>
+                <Content>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('CreateCommunity')}>
+                        <CardItem style={{ marginBottom: -10 }} >
+                            <Card style={{ margin: 50, borderRadius: 8, flexDirection: 'row', alignItems: 'center' }}>
+                                <CardItem style={{ marginLeft: 15 }}>
+                                    <Button style={{ borderRadius: 100, alignItems: 'center', justifyContent: 'center' }} height={65} width={65} >
+                                        <MaterialCommunityIcons
+                                            name='plus'
+                                            color="white"
+                                            size={30}
+                                        />
+                                    </Button>
+                                    <Text style={{ marginLeft: 15, fontWeight: 'bold', fontSize: 25 }}>Create a Group</Text>
+                                </CardItem>
+                                <Right>
+                                </Right>
+                            </Card>
+                        </CardItem>
+                    </TouchableOpacity>
+                    <Button onPress={() => joinGroup(this.state.groupUid[0], this.props.user.uid)}>
+                        <Text>Join Group1</Text>
+                    </Button>
+                    <FlatList
+                        data={this.state.groups}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item, index }) => (
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('CommunityFeed')}>
+                                {/* <Text>{item}</Text> */}
+                                <CommunityCard group_id={item} />
+                            </TouchableOpacity>
 
-                //forEach{
-                    fetchGroupDetails(groups).then( group =>
-                        this.setState({ groups: [...this.state.groups,group] })
-                    )
-                // }
-            }
-        );
-    console.log(this.state.groups)
-    alert(this.state.groups)
-}
-render() {
-    return (
-        <Container>
-            <Content>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('CreateCommunity')}>
-                    <CardItem style={{ marginBottom: -10 }} >
-                        <Card style={{ margin: 50, borderRadius: 8, flexDirection: 'row', alignItems: 'center' }}>
-                            <CardItem style={{ marginLeft: 15 }}>
-                                <Button style={{ borderRadius: 100, alignItems: 'center', justifyContent: 'center' }} height={65} width={65} >
-                                    <MaterialCommunityIcons
-                                        name='plus'
-                                        color="white"
-                                        size={30}
-                                    />
-                                </Button>
-                                <Text style={{ marginLeft: 15, fontWeight: 'bold', fontSize: 25 }}>Create a Group</Text>
-                            </CardItem>
-                            <Right>
-                            </Right>
-                        </Card>
-                    </CardItem>
-                </TouchableOpacity>
-                <Button onPress={() => joinGroup(this.state.groupUid[0], this.props.user.uid)}>
-                    <Text>Join Group1</Text>
-                </Button>
-                <FlatList
-                    data={this.state.groups}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item, index }) => (
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('CommunityFeed')}>
-                            <CardItem style={{ marginVertical: -5 }}>
-                                <Card style={{ padding: 0, borderRadius: 8, flexDirection: 'row', alignItems: 'center' }}
-                                >
-                                    <CardItem style={{ marginLeft: 15 }}>
-                                        <Button style={{ borderRadius: 100, alignItems: 'center', justifyContent: 'center' }} height={65} width={65}
-                                        // onPress={() => this.props.navigation.navigate('CommunityFeed')} 
-                                        >
-                                        </Button>
-                                        <View style={{ marginLeft: 15 }}>
-                                            <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{this.state.groups.name}</Text>
-                                            <Text style={{ color: 'rgba(0,0,0,0.35)' }}>{this.state.groups.members} members</Text>
-                                        </View>
-                                    </CardItem>
-                                    <Right>
-                                    </Right>
-                                </Card>
-                            </CardItem>
-                        </TouchableOpacity>
-
-                    )}
-                />
+                        )}
+                    />
 
 
-            </Content>
-        </Container>
+                </Content>
+            </Container>
 
-    )
-}
+        )
+    }
 
 }
 
